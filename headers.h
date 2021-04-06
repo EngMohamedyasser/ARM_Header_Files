f1ede7be1192d87cd047cb9121988477c2c382fc
Mohamed yasser <Engmohamed_shehab@hotmail.com>
2021-04-06 14:11:55 +0200

    update headers

--- headers.h
+++ headers.h
@@ -7,7 +7,8 @@
 #define PORTF_GPIODEN *((unsigned int *)0x4002551C)
 #define PORTF_GPIODIR *((unsigned int *)0x40025400)
 #define PORTF_GPIODATA *((unsigned int *)0x400253FC)
-#define RED_LED 1
-#define GREEN_LED 2
-#define BLUE_LED 3
+#define RED_LED (1U<<1)
+#define GREEN_LED (1U<<2)
+#define BLUE_LED (1U<<3)
+#define CLOCK_GATING_BIT (1U<<5)
 #endif
